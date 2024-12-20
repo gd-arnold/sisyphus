91fae6d2f4a037ebc12bfdf84487fd1477a1bea6
gd-arnold <gdarnold19@gmail.com>
2024-12-20 10:39:50 +0200

    feat: api: express init

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,8 @@
-console.log("hello");
+import express from "express"
+import "dotenv/config"
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`UP & RUNNING ON PORT ${PORT}`));
