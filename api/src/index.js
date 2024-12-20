6b548a5e43c61464cc3ef6e4abb440174488b1a9
gd-arnold <gdarnold19@gmail.com>
2024-12-20 11:03:37 +0200

    feat: api: cors and json middlewares

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,10 @@
 import express from "express"
+import cors from "cors"
 import { App } from "./config"
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(App.port, () => console.log(`UP & RUNNING ON PORT ${App.port}`));
