cb5959f790033981b7541ad0f88e4dfa343278e7
gd-arnold <gdarnold19@gmail.com>
2025-01-23 22:56:33 +0200

    chore: api: test msg

--- api/src/index.js
+++ api/src/index.js
@@ -2,11 +2,18 @@ import express from "express"
 import cors from "cors"
 import { App } from "./config.js"
 import ApiRouter from "./routes/api.js";
+import os from "os"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    const message = `hello from host ${os.hostname()}`;
+
+    res.status(200).send({ message });
+});
 
 app.use("/api", ApiRouter);
 
