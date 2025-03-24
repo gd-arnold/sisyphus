22306e053147f290afcb27071f5702a7eb0c0b44
gd-arnold <gdarnold19@gmail.com>
2025-03-24 22:45:49 +0200

    feat: client: api interface

---
+++ client/src/utils/api.js
@@ -0,0 +1,42 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+const api = {
+    get: async (url) => {
+        const headers = {
+            "Content-type": "application/json"
+        };
+
+        try {
+            console.log(API_URL);
+            const response = await fetch(API_URL + url, {
+                headers
+            });
+            const data = await response.json();
+            console.log(data);
+
+            return data;
+        } catch (e) {
+            console.error(e);
+        }
+    },
+    post: async (url, body) => {
+        const headers = {
+            "Content-type": "application/json"
+        };
+
+        try {
+            const response = await fetch(API_URL + url, {
+                method: "POST",
+                headers,
+                body
+            });
+            const data = await response.json();
+
+            return data;
+        } catch(e) {
+            console.error(e);
+        }
+    }
+};
+
+export default api;
