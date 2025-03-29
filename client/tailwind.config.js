c76f77f2bcc752109dc16ed8ec2d554e051a72d1
gd-arnold <gdarnold19@gmail.com>
2025-03-29 16:12:52 +0200

    feat: fix: tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            black: "#0F1014",
+            "dark-gray": "#1B1C1F",
+            gray: "#26272B",
+            "light-gray": "#6C7086",
+            blue: "#3564DC",
+            purple: "#4F4CDD",
+        },
+    },
   },
   plugins: [],
 };
