1b0a26533338e1cdea14a45247d9efe351beff6a
gd-arnold <gdarnold19@gmail.com>
2025-03-30 11:24:59 +0300

    feat: client: style header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,53 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../logo.svg"
 import useAuth from "../state/auth";
+import Button from "./Button";
+import { LogOut } from "lucide-react";
 
 function Header() {
     const { logout, isAuthenticated } = useAuth();
     const navigate = useNavigate();
 
     return (
-        <div>
-            <button onClick={() => navigate("/")}>Home</button>
-            {isAuthenticated ? (
-                <button onClick={() => logout()}>Logout</button>
-            ) : (
-                <>
-                <button onClick={() => navigate("/login")}>Login</button>
-                <button onClick={() => navigate("/register")}>Register</button>
-                </>
-            )}
+      <nav className="sticky m-auto flex h-20 w-full items-center justify-between rounded-2xl bg-gray bg-opacity-50 px-8 py-2 backdrop-blur-xl sm:w-[600px]">
+        <Link to="/" className="flex items-center gap-2">
+            <img src={logo} className="size-10" alt="logo" />
+            sisyphus
+        </Link>
+
+        <div className="flex items-center gap-2">
+          {isAuthenticated ? (
+            <>
+              <Button
+                className="cursor-pointer duration-100 hover:opacity-75 flex items-center gap-1"
+                onClick={() => {
+                  logout();
+                }}
+              >
+                <LogOut size={16} />
+                <span className="hidden sm:inline">Logout</span>
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                color="black"
+                className="cursor-pointer text-white duration-100 hover:opacity-75"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </Button>
+
+              <Button
+                className="cursor-pointer duration-100 hover:opacity-90"
+                onClick={() => navigate("/register")}
+              >
+                Sign up
+              </Button>
+            </>
+          )}
         </div>
+      </nav>
     );
 }
 
--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -1,14 +1,15 @@
 import Header from "./Header";
 
 function Page({ children }) {
-    return (
-        <div>
-            <Header />
-            <main>
-                {children}
-            </main>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-8 p-4">
+      <Header />
+
+      <main className="m-auto mt-4 flex w-full flex-col items-center gap-8">
+        {children}
+      </main>
+    </div>
+  );
 }
 
 export default Page;
